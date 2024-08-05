95bf85b9619ae72fd6225bec71dba2119deb5d42
amirjmi <103498744+amirjmi@users.noreply.github.com>
2024-08-05 07:57:43 +0000

    operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,27 @@
+// ***********OPERATIONS*************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-1);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Amir"
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+console.log(+true);
+console.log(+"");
