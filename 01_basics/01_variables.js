f822b002d9748b0dfabf6b2f8d55098db35fedd8
amirjmi <103498744+amirjmi@users.noreply.github.com>
2024-08-05 10:44:23 +0000

    stack and heap memory

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 76475
+let accountEmail = "amir@google.com"
+var accountPassword = "930504"
+accountCity = "Delhi"
+
+
+// accountId = 45
+accountEmail = "am@am.com"
+accountPassword = "323223"
+accountCity = "Bareilly"
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
--- 01_basics/datatype_summary.js
+++ 01_basics/datatype_summary.js
@@ -17,22 +17,74 @@ const anotherId =Symbol('123')
 
 
 
+
+
+
+
+
 // REFERENCE TYPE (NON-PRIMITIVE) DATA TYPES
 // 1-Array
 // 2-Objects
 // 3-Function
 
 // Array declare
-const heros = ["Iron man", "Captain America", "Spiderman"];
+// const heros = ["Iron man", "Captain America", "Spiderman"];
 
 // Object Declare
-let myObj = {
-    name: "Amir",
-    age: 23,
-}
+// let myObj = {
+//     name: "Amir",
+//     age: 23,
+// }
 
 // Function decalre
-const myFunction = function(){
-    console.log("Hello world");
+// const myFunction = function(){
+//     console.log("Hello world");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// *********************Stack and Heap Memory**************
+
+// Stack-Use in primitive data types
+// Heap-Use in non-primitive data type
+
+
+let myYoutubename = "amiridrisidotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+// console.log(myYoutubename);
+// console.log(anothername);
+
+
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
 }
+
+let userTwo = userOne
+
+userTwo.email = "almir@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+
+
+
 
