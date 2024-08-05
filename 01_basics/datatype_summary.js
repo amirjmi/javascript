3405269357eb73b79a416ce7557e5716018a0ff4
amirjmi <103498744+amirjmi@users.noreply.github.com>
2024-08-05 08:47:26 +0000

    datatype_sumary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,38 @@
+// PRIMITIVE DATA TYPES
+// 7 types
+// 1-String,
+// 2-Number,
+// 3-Boolean,
+// 4-null,
+// 5-undefined,
+// 6-Symbol,
+// 7-BigInt
+
+
+// symbol declare
+const id = Symbol('123')
+const anotherId =Symbol('123')
+
+
+
+
+
+// REFERENCE TYPE (NON-PRIMITIVE) DATA TYPES
+// 1-Array
+// 2-Objects
+// 3-Function
+
+// Array declare
+const heros = ["Iron man", "Captain America", "Spiderman"];
+
+// Object Declare
+let myObj = {
+    name: "Amir",
+    age: 23,
+}
+
+// Function decalre
+const myFunction = function(){
+    console.log("Hello world");
+}
+
