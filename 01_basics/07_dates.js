61f844446838532af2a3c53c66cf11509f12c0af
amirjmi <103498744+amirjmi@users.noreply.github.com>
2024-08-06 13:29:39 +0000

    dates methods done

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,27 @@
+// ***************DATES***************
+
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+
+let myCreatedDate = new Date (2025,10,24)
+// console.log(myCreatedDate.toDateString());
+
+// let myCreatedDate = new Date (2025,10,24,6,10)
+// console.log(myCreatedDate.toLocaleString());
+
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
