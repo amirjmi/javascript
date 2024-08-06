c484deafa68db01f06bd1933b78e33288ee14b6a
amirjmi <103498744+amirjmi@users.noreply.github.com>
2024-08-06 08:46:39 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,22 @@
+const name = "Amir"
+const repoCount = 10
+
+// console.log(name + repoCount);
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+
+const gameName = new String('Almir')
+// console.log(gameName.__proto__);
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase);
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('i'));
+
+// const newString = gameName.substring(0,4)
+// console.log(newString);
+// const anotherString = gameName.slice(-4,3)
+
+const newStringOne = "      Almir        "
+console.log(newStringOne);
+console.log(newStringOne.trim);
