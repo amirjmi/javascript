895577342cd2aa328b7d4276721e4a833f749f96
amirjmi <103498744+amirjmi@users.noreply.github.com>
2024-08-09 11:06:31 +0000

    scope done

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,63 @@
+// let a = 10 
+// const b = 20
+// var c = 30
+
+// var c = 300
+let a = 500
+
+if(true){
+    let a = 10 
+    const b = 20
+    // console.log("INNER: ",a)
+    // var c = 30    
+}
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+
+
+// function one(){
+//     const username = "Amir"
+
+//     function two(){
+//         const website = "youtube"
+//         console.log(username);
+//     }
+//     // console.log(website)
+//     two()
+// }
+// one()
+
+
+
+
+// if(true) {
+//     const username = "Amir"
+//     if  (username === "Amir"){
+//         const website = " youtube"
+//         console.log(username + website)
+//     }
+//     console.log(website)
+// }
+// console.log(username)
+
+
+
+
+//************INTRESTING**************//
+
+// console.log(addone(5))
+                            /*this
+function addone(num){      will 
+    return num + 1         give 
+}                          correct output */
+// addone(5)
+ 
+
+// console.log(addTwo(3))           this 
+// const addTwo =  function(num){    will
+//     return num + 2                give 
+// }                                 error
+// addTwo()
