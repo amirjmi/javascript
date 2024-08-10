deb203dea87641d73eec28977502ed69ac797a5b
amirjmi <103498744+amirjmi@users.noreply.github.com>
2024-08-10 11:04:36 +0000

    this and arrow function done

---
+++ 03_basics/03_this&arrow.js
@@ -0,0 +1,71 @@
+// const user = {
+//     username: "Amir",
+//     price: 9999,
+
+//     welcomeMessage: function() {
+//         console.log(`${this.username} , Welcome to Website`);
+//         // console.log(this)
+//     }
+// }
+
+// user.welcomeMessage()
+// user.username = "Sam"
+// user.welcomeMessage()
+
+// console.log(this)
+
+
+
+// function chai(){
+//     let username = "Amir"
+//     console.log(this)
+// }
+// chai()
+
+
+
+// const chai = function(){
+//     let username = "Amir"
+//     console.log(this.username)
+// }
+// chai()
+
+
+
+
+// const chai = () => {
+//     let username = "Amir"
+//     console.log(this.username)
+// }
+// chai()
+
+
+
+
+
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// console.log(addTwo(3, 4))
+
+
+
+// const addTwo = (num1, num2) =>  num1 + num2
+// const addTwo = (num1, num2) =>  (num1 + num2)
+const addTwo = (num1, num2) =>  ({username: "Farish"})
+
+
+
+
+console.log(addTwo(3, 4))
+
+
+
+
+
+
+
+
+
