0864d94337ab161ddf543998329eca5a7d032b6e
amirjmi <103498744+amirjmi@users.noreply.github.com>
2024-08-30 12:22:30 +0000

    for loop done

---
+++ 4/forloop.js
@@ -0,0 +1,65 @@
+// for loop
+// for (let i = 1; i <= 11; i++) {
+//     const element = i;
+//     console.log(element);
+    
+    
+// } 
+
+
+// for (let i = 1; i <= 10; i++) {
+//     const element = i;
+//     if(element==5) {
+//         console.log("5 is best Number");
+//     }
+//     console.log(element)
+    
+    
+// }   
+
+
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`Outer Loop value: ${i}`);
+    
+//     for (let j = 0; j <= 10; j++) {
+//     // console.log(`Inner Loop value: ${j} and inner loop ${i}`);
+//     console.log(i + '*' + j + '=' + i*j);
+//      }
+    
+// }
+
+// let myArray = ["Ironman","Spiderman","Captan America","Dr.Strange","Thor","Antman","Flash","Batman","Superman"]
+// for (let i = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+    
+    
+// }
+
+
+
+
+// Break and Continue
+
+// for (let i = 0; i <= 20; i++) {
+//     if(i==5){
+//         console.log("5 detected")
+//         break
+//     }
+//     console.log(`value of i is ${i}`);
+    
+    
+// }
+
+
+
+for (let i = 0; i <= 20; i++) {
+    if(i==5){
+        console.log("5 detected")
+        continue
+    }
+    console.log(`value of i is ${i}`);
+    
+    
+}
