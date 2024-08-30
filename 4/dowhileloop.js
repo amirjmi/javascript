1cd13c9388590f05f0ef51cddf0efc5ee0210d68
amirjmi <103498744+amirjmi@users.noreply.github.com>
2024-08-30 12:48:51 +0000

    while and do while loop done

---
+++ 4/dowhileloop.js
@@ -0,0 +1,27 @@
+// While loop
+
+// let i = 0;
+// while (i<=10) {
+//     console.log(`Value of i is ${i}`);
+//     i = i + 2
+    
+    
+// }
+
+// let myArray = ["Ironman","Spiderman","Captan America","Dr.Strange","Thor","Antman","Flash","Batman","Superman"]
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`value is ${myArray[arr]}`)
+//     arr = arr + 1
+    
+// }
+
+
+// Do while Loop
+
+let score = 1
+do {
+    console.log(`Score is ${score}`)
+    score++
+    
+} while (score <=10);
