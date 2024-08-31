ace24a567390ccb297bcaf6cb181bd0a6d94fac6
amirjmi <103498744+amirjmi@users.noreply.github.com>
2024-08-31 08:23:17 +0000

    forof,map,forin,foreach done

---
+++ 4/forofloop.js
@@ -0,0 +1,138 @@
+// ***********For Of Loop**************
+
+// for (const element of object) {
+    
+// }
+
+
+// const arr = [1,2,3,4,5]
+// for (const num of arr) {
+//     console.log(num);
+    
+    
+// }
+
+
+// const greetings = "Hello World"
+// for (const greet of greetings) {
+//     console.log(`Each char is ${greet}`);
+    
+    
+// }
+
+
+// **************Map***********
+
+// const map = new Map()
+// map.set('In', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('In', "India")
+// console.log(map);
+
+
+
+// const map = new Map()
+// map.set('In', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('In', "India")
+// for (const [key,value] of map) {
+//     console.log(key, ':-', value);
+    
+    
+// }
+
+
+
+
+// ****************FOR IN LOOP*************
+
+
+// const myObject = {
+//     js: 'Javascript',
+//     cpp: 'C++',
+//     rb: 'Ruby',
+//     chash: 'CSharp'
+// }
+
+// for (const key in myObject) {
+//     console.log(`${key} is shortcut for ${myObject[key]}`);
+    
+        
+//     }
+
+
+
+
+// const programming = ["JS","Rb","Py","Java","CPP"]
+// for (const key in programming) {
+//      console.log(programming[key]);
+     
+// }
+
+
+
+// *************For Each Loop*************
+
+// const coding = ["js","ruby","java","python","cpp"]
+
+// coding.forEach( function (val) {
+//     console.log(val);
+    
+    
+// })
+
+
+// const coding = ["js","ruby","java","python","cpp"]
+
+// coding.forEach( (item) => {
+//     console.log(item);
+    
+    
+// })
+
+
+
+// const coding = ["js","ruby","java","python","cpp"]
+//  coding.forEach( (item,index,arr)=> {
+//     console.log(item,index,arr);
+    
+//  })
+
+
+
+// const myCoding = [
+//     {
+//         languageName: "Javascript",
+//         languageFileName: "js"
+//     },
+
+//     {
+//         languageName: "java",
+//         languageFileName: "java"
+//     },
+
+//     {
+//         languageName: "python",
+//         languageFileName: "py"
+//     },
+// ]
+// myCoding.forEach( (item) => {
+//     console.log(item.languageName);
+    
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
